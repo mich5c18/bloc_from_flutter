54d07d561c6a1d02e62ffcca9171b6cf1ee6b918
mich5c18 <mich5c18@edu.ucl.dk>
2020-05-18 13:45:25 +0200

    Initial commit

---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,54 @@
+import 'package:bloc_from_flutter/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$count',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterEvent.increment);
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterEvent.decrement);
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
